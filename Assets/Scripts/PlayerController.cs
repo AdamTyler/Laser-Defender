7a1b3ea911038721b9bf6c9e815f1a99f9a6ca4e
Adam Tyler <lazy9669@gmail.com>
2017-01-14 11:52:32 -0600

    Adding projectiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 15.0f;
     public float padding;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float fireRate = 0.5f;
 
     float xMin;
     float xMax;
@@ -16,10 +19,25 @@ public class PlayerController : MonoBehaviour {
         xMin = Camera.main.ViewportToWorldPoint (new Vector3(0,0,zDist)).x + padding;
         xMax = Camera.main.ViewportToWorldPoint (new Vector3(1,0,zDist)).x - padding;
 	}
+
+    void Fire ()
+    {
+        GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+        Rigidbody2D rbeam = beam.GetComponent<Rigidbody2D> ();
+        rbeam.velocity = new Vector3(0, projectileSpeed, 0);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            InvokeRepeating ("Fire", 0.000001f, fireRate);
+        }
+
+        if (Input.GetKeyUp (KeyCode.Space)) {
+            CancelInvoke ("Fire");
+        } 
 
         if (Input.GetKey (KeyCode.LeftArrow)) {
 
