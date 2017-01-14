add87e732b64b90f34eb617717ca126be4a8735c
Adam Tyler <lazy9669@gmail.com>
2017-01-14 11:16:23 -0600

    First Comming

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+    public float width = 10f;
+    public float height = 5f;
+
+    string dir = "left";
+
+    public float speed = 5.0f;
+    public float padding;
+
+    float xMin;
+    float xMax;
+
+	// Use this for initialization
+	void Start ()
+    {
+        padding = width / 2;
+        float zDist = transform.position.z - Camera.main.transform.position.z;
+        xMin = Camera.main.ViewportToWorldPoint (new Vector3(0,0,zDist)).x + padding;
+        xMax = Camera.main.ViewportToWorldPoint (new Vector3(1,0,zDist)).x - padding;
+
+        speed = width/ 10;
+
+        foreach (Transform child in transform) {
+
+            GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (dir == "left") {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(transform.position.x >= xMax) {
+            dir = "left";
+        } else if (transform.position.x <= xMin) {
+            dir = "right";
+        }
+	
+	}
+
+    void OnDrawGizmos ()
+    {
+        Gizmos.DrawWireCube (transform.position, new Vector3(width, height));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 15.0f;
+    public float padding;
+
+    float xMin;
+    float xMax;
+
+	// Use this for initialization
+	void Start () {
+        padding = this.GetComponent<Renderer>().bounds.size.x / 2;
+        float zDist = transform.position.z - Camera.main.transform.position.z;
+        xMin = Camera.main.ViewportToWorldPoint (new Vector3(0,0,zDist)).x + padding;
+        xMax = Camera.main.ViewportToWorldPoint (new Vector3(1,0,zDist)).x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        } else if (Input.GetKey (KeyCode.RightArrow)) {
+
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        float newX = Mathf.Clamp (transform.position.x, xMin, xMax);
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	
+	}
+}
