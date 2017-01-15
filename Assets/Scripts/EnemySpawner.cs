607084b180c0aa3cc1347601d1900be160a9a5db
Adam Tyler <lazy9669@gmail.com>
2017-01-15 11:53:14 -0600

    Respawn enemies using recursion

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -12,6 +12,7 @@ public class EnemySpawner : MonoBehaviour {
 
     public float speed = 5.0f;
     public float padding;
+    public float spawnDelay = 0.5f;
 
     float xMin;
     float xMax;
@@ -25,6 +26,11 @@ public class EnemySpawner : MonoBehaviour {
         xMax = Camera.main.ViewportToWorldPoint (new Vector3(1,0,zDist)).x - padding;
 
         speed = width/ 10;
+        SpawnUntilFull ();
+        
+	}
+
+    void SpawnEnemies() {
 
         foreach (Transform child in transform) {
 
@@ -32,8 +38,19 @@ public class EnemySpawner : MonoBehaviour {
             enemy.transform.parent = child;
 
         }
-        
-	}
+    }
+
+    void SpawnUntilFull ()
+    {
+        Transform nextPos = NextFreePosition ();
+        if (nextPos != null) {
+            GameObject enemy = Instantiate (enemyPrefab, nextPos.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = nextPos;
+        }
+        if (NextFreePosition ()) {
+            Invoke ("SpawnUntilFull", spawnDelay);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -45,13 +62,38 @@ public class EnemySpawner : MonoBehaviour {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
 
-        if(transform.position.x >= xMax) {
+        if (transform.position.x >= xMax) {
             dir = "left";
         } else if (transform.position.x <= xMin) {
             dir = "right";
         }
+
+        // check for dead
+        if (AllMembersDead ()) {
+            SpawnUntilFull ();
+        }
 	
 	}
+
+    Transform NextFreePosition() {
+        foreach (Transform child in transform) {
+            if (child.childCount == 0) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    bool AllMembersDead ()
+    {
+        
+        foreach (Transform child in transform) {
+            if (child.childCount > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     void OnDrawGizmos ()
     {
